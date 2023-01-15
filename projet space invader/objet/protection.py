4d2d15f77f9805b97ed75e332be508771b609bbd
nicolas-galliot <114134368+gatilol@users.noreply.github.com>
2023-01-15 14:34:50 +0100

    Update protection.py

--- projet space invader/objet/protection.py
+++ projet space invader/objet/protection.py
@@ -1,8 +1,31 @@
 class protection:
     def __init__(self,canvas):
-        self.protection1=[]
-        self.protection2=[]
-        self.protection3=[]
+        self.protection_coord_x_base=50
+        self.protection_coord_y_base=400
+        self.protection_hauteur=40
+        self.protection_largeur=40
+        self.bloc=[]
+        self.tbloc=[]
+        self.fullprotect=[]
 
-    #def creation_protection(self,canvas):
-    #4bloc de 100 à 200 puis 300 à 400 puis 500 à 600
+    def creation_protection(self,canvas):
+        for bloc in range (2):
+            self.protection_coord_y=self.protection_coord_y_base
+            
+
+            for i in range(2):
+                self.protection_coord_x=self.protection_coord_x_base
+                self.protection_coord_y=self.protection_coord_y+50
+                for v in range(4):
+                    self.protection_coord_x=self.protection_coord_x+45
+                    self.pro_id=canvas.create_rectangle(self.protection_coord_x,
+                                        self.protection_coord_y,
+                                        self.protection_coord_x+self.protection_largeur,
+                                        self.protection_coord_y+self.protection_hauteur,
+                                        outline="#000000", fill="#FF0000")
+                    self.bloc.append(self.pro_id)
+                self.tbloc.append(self.bloc)
+            (x_p_0,y_p_0,x_p_1,y_p_1)=canvas.coords(self.bloc[-1])
+            self.protection_coord_x_base=x_p_1+100
+            self.fullprotect.append(self.tbloc)
+        return self.fullprotect
